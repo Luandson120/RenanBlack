68f685b31702a24f99f139967ae20acbae445862
luandson120 <seu-email@gmail.com>
2026-04-24 15:02:31 -0300

    feat: criação do banco de dados

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,11 @@
-import "dotenv/config";
+// prisma.config.ts
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+   seed: "npx tsx ./prisma/seed.ts",
   },
-
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
 });
